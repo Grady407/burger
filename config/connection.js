f4d11144f3444d4960257751faeddb832ca807f6
grady407 <grady407@gmail.com>
2018-02-08 17:52:08 -0500

    update_update

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,28 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "mcg321407",
-  database: "burgers_db"
-});
+//******************added for heroku************
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'bootcamp',
+    database: 'burgers_db'
+  });
+};
+
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "bootcamp",
+//   database: "burgers_db"
+// });
 
 
 connection.connect(function(err) {
